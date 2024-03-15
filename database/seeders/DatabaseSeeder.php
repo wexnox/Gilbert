9ba7f0dff22f96d63e4882d0646a191e52820f74
Lasse R <lasseronnlie@hotmail.com>
2024-03-15 18:03:39 +0100

    *

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,21 +2,22 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
-     *
-     * @return void
      */
-    public function run()
+    public function run(): void
     {
-        $this->call(RecipeSeeder::class);
-        $this->call(IngredientSeeder::class);
-        $this->call(ShoppingCartSeeder::class);
-        $this->call(PermissionsSeeder::class);
+        // User::factory(10)->create();
 
+        User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+        ]);
     }
 }
