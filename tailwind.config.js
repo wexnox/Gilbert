1622754146a6eed5d7a267515d68ea2394971d07
Lasse R <lasseronnlie@hotmail.com>
2024-03-28 07:40:35 +0100

    Add new Tailwind CSS configuration file
    The new `tailwind.config.js` file has been created with basic configuration. It has been set up with empty "content", "theme extend", and "plugins" arrays, which are ready for customization as per future needs. This file is set to purge unnecessary styles from Blade, JavaScript, and Vue files in the resources directory.

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    purge: [
+        './resources/**/*.blade.php',
+        './resources/**/*.js',
+        './resources/**/*.vue',
+    ],
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
