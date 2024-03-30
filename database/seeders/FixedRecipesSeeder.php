f9c044e805c60f58e3273b32edc57046b13a7f95
Lasse R <lasseronnlie@hotmail.com>
2024-03-30 14:46:51 +0100

    Add FixedRecipesSeeder for database seeding
    Added a new Seeder, FixedRecipesSeeder, for creating predefined recipes and their associated ingredients in the database. This will facilitate the testing and development process by allowing to populate the database with standard data. Seeder includes features for managing ingredients and recipes data such as preparation steps, serving size, and cooking time.

---
+++ database/seeders/FixedRecipesSeeder.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class FixedRecipesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run()
+    {
+        $recipes = [
+            [
+                'title' => 'Recipe 1',
+                'description' => 'Description for Recipe 1',
+                'preparation_steps' => 'Step 1, Step 2, Step 3',
+                'serving_size' => 4,
+                'cooking_time' => 30,
+                'image' => 'path/to/image1.jpg',
+                'ingredients' => [
+                    ['name' => 'Ingredient 1', 'type' => 'Vegetable', 'unit_of_measurement' => 'cups', 'quantity' => 2, 'measurement' => 'cups'],
+                    // Add more ingredients for Recipe 1
+                ],
+            ],
+            // Define more recipes
+        ];
+
+        foreach ($recipes as $recipeData) {
+            $recipe = Recipe::create(Arr::except($recipeData, ['ingredients']));
+
+            foreach ($recipeData['ingredients'] as $ingredientData) {
+                $ingredient = Ingredient::firstOrCreate(
+                    Arr::only($ingredientData, ['name', 'type', 'unit_of_measurement'])
+                );
+                $recipe->ingredients()->attach($ingredient->id, Arr::only($ingredientData, ['quantity', 'measurement']));
+            }
+        }
+    }
+}
