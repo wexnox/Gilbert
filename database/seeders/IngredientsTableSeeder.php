840a7879a8cac65c93a199ac67c48cda5fae4060
Lasse R <lasseronnlie@hotmail.com>
2024-12-12 08:58:00 +0100

    Update IngredientsTableSeeder to include detailed data
    Expanded the seeder with a comprehensive list of ingredients, including detailed nutrient and allergen information. This improves the database seeding process for more accurate and diverse testing scenarios.

--- database/seeders/IngredientsTableSeeder.php
+++ database/seeders/IngredientsTableSeeder.php
@@ -13,24 +13,193 @@ class IngredientsTableSeeder extends Seeder
      */
     public function run(): void
     {
-//        Ingredient::factory()->count(10)->create();
+        $ingredients = [
+            [
+                'name' => 'Flour',
+                'allergen_info' => json_encode(['gluten']),
+                'nutrients' => json_encode([
+                    'calories' => 364,
+                    'carbohydrates' => 76,
+                    'protein' => 10,
+                    'fat' => 1,
+                    'fiber' => 2,
+                    'calcium' => 15,
+                    'iron' => 5
+                ])
+            ],
+            [
+                'name' => 'Sugar',
+                'allergen_info' => json_encode([]),
+                'nutrients' => json_encode([
+                    'calories' => 387,
+                    'carbohydrates' => 100,
+                    'protein' => 0,
+                    'fat' => 0,
+                    'fiber' => 0,
+                    'calcium' => 1,
+                    'iron' => 0
+                ])
+            ],
+            [
+                'name' => 'Egg',
+                'allergen_info' => json_encode(['egg']),
+                'nutrients' => json_encode([
+                    'calories' => 68,
+                    'carbohydrates' => 1,
+                    'protein' => 6,
+                    'fat' => 5,
+                    'fiber' => 0,
+                    'calcium' => 25,
+                    'iron' => 1
+                ])
+            ],
+            [
+                'name' => 'Milk',
+                'allergen_info' => json_encode(['dairy']),
+                'nutrients' => json_encode([
+                    'calories' => 42,
+                    'carbohydrates' => 5,
+                    'protein' => 3,
+                    'fat' => 1,
+                    'fiber' => 0,
+                    'calcium' => 113,
+                    'iron' => 0
+                ])
+            ],
+            [
+                'name' => 'Butter',
+                'allergen_info' => json_encode(['dairy']),
+                'nutrients' => json_encode([
+                    'calories' => 717,
+                    'carbohydrates' => 0,
+                    'protein' => 1,
+                    'fat' => 81,
+                    'fiber' => 0,
+                    'calcium' => 24,
+                    'iron' => 0
+                ])
+            ],
+            [
+                'name' => 'Salt',
+                'allergen_info' => json_encode([]),
+                'nutrients' => json_encode([
+                    'calories' => 0,
+                    'carbohydrates' => 0,
+                    'protein' => 0,
+                    'fat' => 0,
+                    'fiber' => 0,
+                    'calcium' => 0,
+                    'iron' => 0
+                ])
+            ],
+            [
+                'name' => 'Olive Oil',
+                'allergen_info' => json_encode([]),
+                'nutrients' => json_encode([
+                    'calories' => 884,
+                    'carbohydrates' => 0,
+                    'protein' => 0,
+                    'fat' => 100,
+                    'fiber' => 0,
+                    'calcium' => 1,
+                    'iron' => 0
+                ])
+            ],
+            [
+                'name' => 'Chicken Breast',
+                'allergen_info' => json_encode([]),
+                'nutrients' => json_encode([
+                    'calories' => 165,
+                    'carbohydrates' => 0,
+                    'protein' => 31,
+                    'fat' => 3,
+                    'fiber' => 0,
+                    'calcium' => 13,
+                    'iron' => 1
+                ])
+            ],
+            [
+                'name' => 'Rice',
+                'allergen_info' => json_encode([]),
+                'nutrients' => json_encode([
+                    'calories' => 130,
+                    'carbohydrates' => 28,
+                    'protein' => 2,
+                    'fat' => 0,
+                    'fiber' => 1,
+                    'calcium' => 10,
+                    'iron' => 1
+                ])
+            ],
+            [
+                'name' => 'Carrot',
+                'allergen_info' => json_encode([]),
+                'nutrients' => json_encode([
+                    'calories' => 41,
+                    'carbohydrates' => 10,
+                    'protein' => 1,
+                    'fat' => 0,
+                    'fiber' => 3,
+                    'calcium' => 33,
+                    'iron' => 0
+                ])
+            ],
+            [
+                'name' => 'Potato',
+                'allergen_info' => json_encode([]),
+                'nutrients' => json_encode([
+                    'calories' => 77,
+                    'carbohydrates' => 17,
+                    'protein' => 2,
+                    'fat' => 0,
+                    'fiber' => 2,
+                    'calcium' => 10,
+                    'iron' => 1
+                ])
+            ],
+            [
+                'name' => 'Tomato',
+                'allergen_info' => json_encode([]),
+                'nutrients' => json_encode([
+                    'calories' => 18,
+                    'carbohydrates' => 4,
+                    'protein' => 1,
+                    'fat' => 0,
+                    'fiber' => 1,
+                    'calcium' => 10,
+                    'iron' => 0
+                ])
+            ],
+            [
+                'name' => 'Cheese',
+                'allergen_info' => json_encode(['dairy']),
+                'nutrients' => json_encode([
+                    'calories' => 402,
+                    'carbohydrates' => 1,
+                    'protein' => 25,
+                    'fat' => 33,
+                    'fiber' => 0,
+                    'calcium' => 721,
+                    'iron' => 0
+                ])
+            ],
+            [
+                'name' => 'Banana',
+                'allergen_info' => json_encode([]),
+                'nutrients' => json_encode([
+                    'calories' => 89,
+                    'carbohydrates' => 23,
+                    'protein' => 1,
+                    'fat' => 0,
+                    'fiber' => 3,
+                    'calcium' => 5,
+                    'iron' => 0
+                ])
+            ]
+        ];
 
-        Ingredient::create([
-            'name' => 'Flour',
-            'allergen_info' => json_encode(['gluten']),
-            'nutrients' => json_encode(['calories' => 100, 'carbohydrates' => 22])
-        ]);
-
-        Ingredient::create([
-            'name' => 'Sugar',
-            'allergen_info' => json_encode([]),
-            'nutrients' => json_encode(['calories' => 387, 'carbohydrates' => 100])
-        ]);
-
-        Ingredient::create([
-            'name' => 'Egg',
-            'allergen_info' => json_encode(['egg']),
-            'nutrients' => json_encode(['protein' => 6, 'fat' => 5])
-        ]);
+        foreach ($ingredients as $ingredient) {
+            Ingredient::create($ingredient);
+        }
     }
 }
