3254614f874d8a5f2d97c83301edb3f4b1d78e69
Lasse R <lasseronnlie@hotmail.com>
2024-12-17 17:46:31 +0100

    Update FixedRecipesSeeder with new Klissekake recipe
    Replaced placeholder recipe with detailed "Klissekake (brownies)" recipe. Includes updated title, description, preparation steps, cooking time, and a comprehensive list of ingredients. This improves the seeder's utility and provides a complete example for testing.

--- database/seeders/FixedRecipesSeeder.php
+++ database/seeders/FixedRecipesSeeder.php
@@ -14,20 +14,65 @@ class FixedRecipesSeeder extends Seeder
     {
         $recipes = [
             [
-                'title' => 'Recipe 1',
-                'description' => 'Description for Recipe 1',
+                'title' => 'Klissekake (brownies )',
+                'description' => 'Digg, fuktig og seig brownies.',
 //                'preparation_steps' => 'Step 1, Step 2, Step 3',
                 'preparation_steps' => json_encode([
-                    "Step 1 for Recipe 1",
-                    "Step 2 for Recipe 1",
+                    "Sett ovnen på 150 grader.",
+                    "Smelt smøret ferdig i en kjele og avkjøl bitte litte granne.",
+                    "Bland alt det tørre i en bolle og ta smelta smøret i det tørre og bland alt sammen.",
+                    "Pisk inn eggene, ett om gangen (du trenger ikke overpiske, bare du ser det har blitt blandet ordentlig).",
+                    "Ta melis tilslutt i deigen (hvis du vil). Bre deigen i en bakepapirkledd langpanne.",
+                    "Stek kaken midt i ovnen på 45 minutter.",
                     // Additional steps as needed
                 ]),
                 'serving_size' => 4,
-                'cooking_time' => 30,
+                'cooking_time' => 45,
                 'image' => 'path/to/image1.jpg',
                 'ingredients' => [
-                    ['name' => 'Ingredient 1', 'type' => 'Vegetable', 'unit_of_measurement' => 'cups', 'quantity' => 2, 'measurement' => 'cups'],
-                    // Add more ingredients for Recipe 1
+                    [
+                        'name' => '300 g smør/ margarin',
+                        'type' => 'Vegetable',
+                        'unit_of_measurement' => 'grams',
+                        'quantity' => 300,
+                        'measurement' => 'grams'
+                    ],
+                    [
+                        'name' => '9 dl sukker',
+                        'type' => 'Fruit',
+                        'unit_of_measurement' => 'dl',
+                        'quantity' => 9,
+                        'measurement' => 'dl'
+                    ],
+                    [
+                        'name' => '6 egg',
+                        'type' => 'Fruit',
+                        'unit_of_measurement' => 'pieces',
+                        'quantity' => 6,
+                        'measurement' => 'pieces'
+                    ],
+                    [
+                        'name' => '4 1/2 dl hvetemel',
+                        'type' => 'Fruit',
+                        'unit_of_measurement' => 'dl',
+                        'quantity' => 4.5,
+                        'measurement' => 'dl'
+                    ],
+                    [
+                        'name' => '9-12 ss kakao',
+                        'type' => 'Fruit',
+                        'unit_of_measurement' => 'pieces',
+                        'quantity' => 2,
+                        'measurement' => 'pieces'
+                    ],
+                    [
+                        'name' => '6 ts vaniljesukker',
+                        'type' => 'Fruit',
+                        'unit_of_measurement' => 'pieces',
+                        'quantity' => 2,
+                        'measurement' => 'pieces'
+                    ]
+
                 ],
             ],
             // Define more recipes
